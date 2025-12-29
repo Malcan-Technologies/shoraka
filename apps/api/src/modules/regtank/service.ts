bf7fece57f98c01ec48e3926889180ca1b0d33be
Lucas <denglucasyijin@gmail.com>
2025-12-29 12:54:59 +0800

    fix: update livenessConfidence to 70

--- apps/api/src/modules/regtank/service.ts
+++ apps/api/src/modules/regtank/service.ts
@@ -133,7 +133,7 @@ export class RegTankService {
       try {
         await this.apiClient.setOnboardingSettings({
           formId,
-          livenessConfidence: 90,
+          livenessConfidence: 70,
           approveMode: true,
           kycApprovalTarget: "ACURIS",
           enabledRegistrationEmail: false,
@@ -240,7 +240,7 @@ export class RegTankService {
     try {
       await this.apiClient.setOnboardingSettings({
         formId,
-        livenessConfidence: 90,
+        livenessConfidence: 70,
         approveMode: true,
         kycApprovalTarget: "ACURIS",
         enabledRegistrationEmail: false,
@@ -508,7 +508,7 @@ export class RegTankService {
         try {
           await this.apiClient.setOnboardingSettings({
             formId: formIdToUse,
-            livenessConfidence: 90,
+            livenessConfidence: 70,
             approveMode: true,
             kycApprovalTarget: "ACURIS",
             enabledRegistrationEmail: false,
@@ -567,7 +567,7 @@ export class RegTankService {
     try {
       await this.apiClient.setOnboardingSettings({
         formId: formIdToUse,
-        livenessConfidence: 90,
+        livenessConfidence: 70,
         approveMode: true,
         kycApprovalTarget: "ACURIS",
         enabledRegistrationEmail: false,
@@ -1925,7 +1925,7 @@ export class RegTankService {
     try {
       await this.apiClient.setOnboardingSettings({
         formId,
-        livenessConfidence: organization.type === OrganizationType.PERSONAL ? 90 : 90,
+        livenessConfidence: 70,
         approveMode: true,
         kycApprovalTarget: "ACURIS",
         enabledRegistrationEmail: false,
