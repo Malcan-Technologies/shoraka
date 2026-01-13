6009f55e6ae95629ad9bfbe9710a3c670f831d8e
Lucas Deng <lucas.deng@malcan.io>
2026-01-13 10:53:20 +0800

    fix: migration

---
+++ apps/api/prisma/migrations/20260113025231_restore_director_status_columns/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "investor_organizations" ADD COLUMN     "director_aml_status" JSONB,
+ADD COLUMN     "director_kyc_status" JSONB;
+
+-- AlterTable
+ALTER TABLE "issuer_organizations" ADD COLUMN     "director_aml_status" JSONB,
+ADD COLUMN     "director_kyc_status" JSONB;
