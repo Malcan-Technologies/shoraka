27020fb52515fd340a65adb70820b03776dd29da
Lucas Deng <lucas.deng@malcan.io>
2026-01-16 15:49:48 +0800

    fix: migration error

--- apps/api/prisma/migrations/20260116072622_/migration.sql
+++ apps/api/prisma/migrations/20260116072622_/migration.sql
@@ -7,9 +7,30 @@
 -- AlterEnum
 BEGIN;
 CREATE TYPE "OrganizationMemberRole_new" AS ENUM ('ORGANIZATION_ADMIN', 'ORGANIZATION_MEMBER');
-ALTER TABLE "organization_members" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING ("role"::text::"OrganizationMemberRole_new");
-ALTER TABLE "investor_organization_invitations" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING ("role"::text::"OrganizationMemberRole_new");
-ALTER TABLE "issuer_organization_invitations" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING ("role"::text::"OrganizationMemberRole_new");
+ALTER TABLE "organization_members" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING (
+  CASE "role"::text
+    WHEN 'OWNER' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
+    WHEN 'DIRECTOR' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
+    WHEN 'MEMBER' THEN 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+    ELSE 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+  END
+);
+ALTER TABLE "investor_organization_invitations" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING (
+  CASE "role"::text
+    WHEN 'OWNER' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
+    WHEN 'DIRECTOR' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
+    WHEN 'MEMBER' THEN 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+    ELSE 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+  END
+);
+ALTER TABLE "issuer_organization_invitations" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING (
+  CASE "role"::text
+    WHEN 'OWNER' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
+    WHEN 'DIRECTOR' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
+    WHEN 'MEMBER' THEN 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+    ELSE 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+  END
+);
 ALTER TYPE "OrganizationMemberRole" RENAME TO "OrganizationMemberRole_old";
 ALTER TYPE "OrganizationMemberRole_new" RENAME TO "OrganizationMemberRole";
 DROP TYPE "OrganizationMemberRole_old";
