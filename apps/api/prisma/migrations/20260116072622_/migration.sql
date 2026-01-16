33cc74764ba54a11098a48aecb4fcf4b77e1ba58
Lucas Deng <lucas.deng@malcan.io>
2026-01-16 15:57:35 +0800

    fix: migration issue

--- apps/api/prisma/migrations/20260116072622_/migration.sql
+++ apps/api/prisma/migrations/20260116072622_/migration.sql
@@ -5,33 +5,31 @@
 
 */
 -- AlterEnum
-BEGIN;
 CREATE TYPE "OrganizationMemberRole_new" AS ENUM ('ORGANIZATION_ADMIN', 'ORGANIZATION_MEMBER');
 ALTER TABLE "organization_members" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING (
   CASE "role"::text
-    WHEN 'OWNER' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
-    WHEN 'DIRECTOR' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
-    WHEN 'MEMBER' THEN 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
-    ELSE 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+    WHEN 'OWNER' THEN CAST('ORGANIZATION_ADMIN' AS "OrganizationMemberRole_new")
+    WHEN 'DIRECTOR' THEN CAST('ORGANIZATION_ADMIN' AS "OrganizationMemberRole_new")
+    WHEN 'MEMBER' THEN CAST('ORGANIZATION_MEMBER' AS "OrganizationMemberRole_new")
+    ELSE CAST('ORGANIZATION_MEMBER' AS "OrganizationMemberRole_new")
   END
 );
 ALTER TABLE "investor_organization_invitations" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING (
   CASE "role"::text
-    WHEN 'OWNER' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
-    WHEN 'DIRECTOR' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
-    WHEN 'MEMBER' THEN 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
-    ELSE 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+    WHEN 'OWNER' THEN CAST('ORGANIZATION_ADMIN' AS "OrganizationMemberRole_new")
+    WHEN 'DIRECTOR' THEN CAST('ORGANIZATION_ADMIN' AS "OrganizationMemberRole_new")
+    WHEN 'MEMBER' THEN CAST('ORGANIZATION_MEMBER' AS "OrganizationMemberRole_new")
+    ELSE CAST('ORGANIZATION_MEMBER' AS "OrganizationMemberRole_new")
   END
 );
 ALTER TABLE "issuer_organization_invitations" ALTER COLUMN "role" TYPE "OrganizationMemberRole_new" USING (
   CASE "role"::text
-    WHEN 'OWNER' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
-    WHEN 'DIRECTOR' THEN 'ORGANIZATION_ADMIN'::OrganizationMemberRole_new
-    WHEN 'MEMBER' THEN 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
-    ELSE 'ORGANIZATION_MEMBER'::OrganizationMemberRole_new
+    WHEN 'OWNER' THEN CAST('ORGANIZATION_ADMIN' AS "OrganizationMemberRole_new")
+    WHEN 'DIRECTOR' THEN CAST('ORGANIZATION_ADMIN' AS "OrganizationMemberRole_new")
+    WHEN 'MEMBER' THEN CAST('ORGANIZATION_MEMBER' AS "OrganizationMemberRole_new")
+    ELSE CAST('ORGANIZATION_MEMBER' AS "OrganizationMemberRole_new")
   END
 );
 ALTER TYPE "OrganizationMemberRole" RENAME TO "OrganizationMemberRole_old";
 ALTER TYPE "OrganizationMemberRole_new" RENAME TO "OrganizationMemberRole";
 DROP TYPE "OrganizationMemberRole_old";
-COMMIT;
