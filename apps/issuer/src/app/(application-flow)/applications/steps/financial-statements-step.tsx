18f1406b180cc32f3eeb4127a4c9789c8625e43f
Max Chng <max.chng@truestack.my>
2026-09-19 14:30:17 +0800

    fix ui add space on financial

--- apps/issuer/src/app/(application-flow)/applications/steps/financial-statements-step.tsx
+++ apps/issuer/src/app/(application-flow)/applications/steps/financial-statements-step.tsx
@@ -1180,11 +1180,11 @@ export function FinancialStatementsStep({
                   id={`${yearKey}-${key}`}
                   label={
                     key === "equity_share_application"
-                      ? "Share Application Account"
+                      ? "Share Application Account "
                       : key === "equity_share_premium"
-                        ? "Share Premium & Other Reserves"
+                        ? "Share Premium & Other Reserves "
                         : key === "equity_minority"
-                          ? "Minority Interest"
+                          ? "Minority Interest "
                           : getLabel(key)
                   }
                   value={form[key] ?? ""}
